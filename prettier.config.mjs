a1ea703b1da1f7873fe2d7ab7335fa8713ddc6a4
Eric Fennis <eric.fennis@gmail.com>
2026-03-26 10:47:58 +0100

    add biome configuration

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -4,14 +4,14 @@ const config = {
   trailingComma: 'all',
   printWidth: 100,
   singleAttributePerLine: true,
-  overrides: [
-    {
-      files: ['icons/*.json', 'categories/*.json'],
-      options: {
-        printWidth: 0,
-      },
-    },
-  ],
+  // overrides: [
+  //   {
+  //     files: ['icons/*.json', 'categories/*.json'],
+  //     options: {
+  //       printWidth: 0,
+  //     },
+  //   },
+  // ],
 };
 
 export default config;
